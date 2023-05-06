2b14aeb62fbc3a0e4fa5c5cba8044029f5cc0ad8
vishalMore <vishal.more@rsllearning.com>
2023-05-07 00:35:14 +0530

    second commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import bgImg from '../assets/images/about-hero.png'
 export default function About() {
   return (
     <div className="about-page-container">
-      <img src={bgImg} className="about-hero-image" />
+      <img src={bgImg} className="about-hero-image" alt="bg-img"/>
       <div className="about-page-content">
         <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
         <p>
